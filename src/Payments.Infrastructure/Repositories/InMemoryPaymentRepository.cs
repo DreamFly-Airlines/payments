adf840ccdb311ab3d495b6f179834e36d93345e9
ArtemToporkov <toporkovartem259@gmail.com>
2025-09-25 13:47:19 +0500

    feat: add in-memory repository for Payments

---
+++ src/Payments.Infrastructure/Repositories/InMemoryPaymentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Payments.Application.Abstractions;
+using Payments.Domain.AggregateRoots;
+using Payments.Domain.Repositories;
+
+namespace Payments.Infrastructure.Repositories;
+
+public class InMemoryPaymentRepository(IEventPublisher eventPublisher) : IPaymentRepository
+{
+    private static readonly Dictionary<string, Payment> Payments = new();
+    public Task<Payment?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
+    {
+        var payment = Payments.GetValueOrDefault(id);
+        return Task.FromResult(payment);
+    }
+
+    public async Task AddAsync(string id, Payment payment, CancellationToken cancellationToken = default)
+    {
+        if (!Payments.TryAdd(id, payment))
+            throw new Exception();
+        await SaveChangesAsync(payment, cancellationToken);
+        payment.ClearDomainEvents();
+    }
+
+    public Task RemoveAsync(Payment payment, CancellationToken cancellationToken = default)
+    {
+        Payments.Remove(payment.PaymentId);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync(Payment payment, CancellationToken cancellationToken = default)
+    {
+        foreach (var @event in payment.DomainEvents)
+            await eventPublisher.PublishAsync(@event, cancellationToken);
+        payment.ClearDomainEvents();
+    }
+}
