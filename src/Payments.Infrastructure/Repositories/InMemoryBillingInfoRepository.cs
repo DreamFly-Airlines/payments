ed6278894ac575fb3e018dee06f40a47212d3b55
ArtemToporkov <toporkovartem259@gmail.com>
2025-09-25 19:52:24 +0500

    feat: add in-memory IBillingInfoRepository impl

---
+++ src/Payments.Infrastructure/Repositories/InMemoryBillingInfoRepository.cs
@@ -0,0 +1,35 @@
+﻿using Payments.Domain.Entities;
+using Payments.Domain.Repositories;
+using Payments.Domain.ValueObjects;
+
+namespace Payments.Infrastructure.Repositories;
+
+public class InMemoryBillingInfoRepository : IBillingInfoRepository
+{
+    private static readonly Dictionary<(string UserId, PaymentChannel PaymentChannel), BillingInfo> 
+        BillingInfos = new();
+    public Task<BillingInfo?> GetAsync(
+        string userId, PaymentChannel paymentChannel, CancellationToken cancellationToken = default)
+    {
+        var billingInfo = BillingInfos.GetValueOrDefault((userId, paymentChannel));
+        return Task.FromResult(billingInfo);
+    }
+
+    public async Task AddAsync(BillingInfo billingInfo, CancellationToken cancellationToken = default)
+    {
+        if (!BillingInfos.TryAdd((billingInfo.UserId, billingInfo.Channel), billingInfo))
+            throw new Exception();
+        await SaveChangesAsync(billingInfo, cancellationToken);
+    }
+
+    public Task RemoveAsync(BillingInfo billingInfo, CancellationToken cancellationToken = default)
+    {
+        BillingInfos.Remove((billingInfo.UserId, billingInfo.Channel));
+        return Task.CompletedTask;
+    }
+
+    public Task SaveChangesAsync(BillingInfo billingInfo, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+}
