830e2bf954ca3013776bbedc30e876b78f4de907
ArtemToporkov <toporkovartem259@gmail.com>
2025-09-25 19:51:57 +0500

    feat: implement event publisher

---
+++ src/Payments.Infrastructure/Events/ServiceProviderEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Payments.Application.Abstractions;
+
+namespace Payments.Infrastructure.Events;
+
+public class ServiceProviderEventPublisher(IServiceProvider serviceProvider) : IEventPublisher
+{
+    public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
+    {
+        var handlers = serviceProvider.GetServices<IEventHandler<TEvent>>();
+        foreach (var handler in handlers)
+            await handler.HandleAsync(@event, cancellationToken);
+    }
+}
